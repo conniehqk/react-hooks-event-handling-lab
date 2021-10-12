7a22d9cc08928bec053a378381df2cccd87d6139
conniekang <hqk1121@gmail.com>
2021-10-12 15:27:55 -0400

    commit tree 00d7de8c8285e01b580874b325c607569270061b

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+function Keypad() {
+    const changePW=()=>{
+        console.log('Entering password...')
+    }
+    return (
+        <input type="password" onChange={changePW}></input>
+    )
+}
+
+export default Keypad
